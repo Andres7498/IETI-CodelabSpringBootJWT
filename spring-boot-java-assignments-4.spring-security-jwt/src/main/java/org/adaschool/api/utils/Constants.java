537b045c52c01c9c0b6f2de7830693053951c1b5
Andres <rixco1998@hotmail.com>
2023-02-28 23:19:32 -0500

    JWTfilter

---
+++ spring-boot-java-assignments-4.spring-security-jwt/src/main/java/org/adaschool/api/utils/Constants.java
@@ -0,0 +1,15 @@
+package org.adaschool.api.utils;
+
+public interface Constants {
+
+    String CLAIMS_ROLES_KEY = "ada_roles";
+
+    int TOKEN_DURATION_MINUTES = 1440;
+    public static final String COOKIE_NAME = "";
+
+    String ADMIN_ROLE = "ADMIN";
+    String USER_ROLE = "USER";
+
+    String MISSING_TOKEN_ERROR_MESSAGE = "Missing or wrong token";
+    String TOKEN_EXPIRED_MALFORMED_ERROR_MESSAGE = "Token expired or malformed";
+}
